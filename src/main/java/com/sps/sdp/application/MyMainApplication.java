30407534546e7ec24ede189afebf966b44dc4952
SPS <varadaraj2103@gmail.com>
2016-11-27 11:42:40 +0530

    Initial commit

---
+++ src/main/java/com/sps/sdp/application/MyMainApplication.java
@@ -0,0 +1,12 @@
+package com.sps.sdp.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMainApplication.class, args);
+	}
+}
