8b472b0e0ccaeec146c5e365e150101bf13d4546
SPS <varadaraj2103@gmail.com>
2016-11-28 23:29:32 +0530

    Started implementing DevConfiguration

---
+++ src/main/java/com/sps/sdp/configuration/DevConfiguration.java
@@ -0,0 +1,13 @@
+package com.sps.sdp.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile(value="default")
+public class DevConfiguration {
+
+	
+	
+	
+}
